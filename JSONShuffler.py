0c7bf924b39ee1514280b3a49c633ed989b5b7eb
zboerger2 <zennith.boerger@gmail.com>
2023-04-10 16:07:39 -0500

    added some boiler plate code for working with ijson

--- JSONShuffler.py
+++ JSONShuffler.py
@@ -5,13 +5,16 @@ __author__ = "Zennith Boerger"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-_ENEMYCOUNT = 18027
+_ENEMYCOUNT = (18028 - 15184)
+_ENEMYDISPLACEMENT = 15184
 
 def main():
     f = open(r'C:\Users\Zenni\Documents\GitHub\yakuza-7-randomizer\Test JSON\character_npc_soldier_personal_data.bin.json', r'r')
     # there are 18027 npc's
-    for i in len(range(30)):
-        print(ijson.items(f, '${i}'))
+    for i in range(_ENEMYCOUNT):
+        index = i + _ENEMYDISPLACEMENT
+        soldier = ijson.items(f, '{}.item'.format(index))
+        print(soldier)
     ## parse JSON file, obtain dictionary of all valid soldier npcs
     ## then shuffle order of dictionary and put the npcs back into the json file
 
