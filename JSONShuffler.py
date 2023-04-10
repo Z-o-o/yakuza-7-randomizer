67c167b0b46b4cc7f87b0a4c40e0bae81d2f9ac0
zboerger2 <zennith.boerger@gmail.com>
2023-04-10 08:09:35 -0500

    import ijson library in JSONShuffler

--- JSONShuffler.py
+++ JSONShuffler.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import json
+import ijson
 
 __author__ = "Zennith Boerger"
 __version__ = "0.1.0"
@@ -10,8 +10,6 @@ def main():
     json_file = open("character_npc_soldier_personal_data.bin.json", "r")
     ## parse JSON file, obtain dictionary of all valid soldier npcs
     ## then shuffle order of dictionary and put the npcs back into the json file
-    y = json.loads(json_file.read())
-    print(y)
 
 
 if __name__ == "__main__":
