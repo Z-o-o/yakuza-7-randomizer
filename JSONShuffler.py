b746cfa525d1e7fc4f164edd267e216b10c983d4
zboerger2 <zennith.boerger@gmail.com>
2023-04-10 14:31:12 -0500

    Begin implementation to pull in the data. Added test JSON folder for easy access to json file.

--- JSONShuffler.py
+++ JSONShuffler.py
@@ -5,9 +5,13 @@ __author__ = "Zennith Boerger"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+_ENEMYCOUNT = 18027
 
 def main():
-    json_file = open("character_npc_soldier_personal_data.bin.json", "r")
+    f = open(r'C:\Users\Zenni\Documents\GitHub\yakuza-7-randomizer\Test JSON\character_npc_soldier_personal_data.bin.json', r'r')
+    # there are 18027 npc's
+    for i in len(range(30)):
+        print(ijson.items(f, '${i}'))
     ## parse JSON file, obtain dictionary of all valid soldier npcs
     ## then shuffle order of dictionary and put the npcs back into the json file
 
