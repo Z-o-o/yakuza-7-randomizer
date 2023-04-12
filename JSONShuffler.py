fbe210bdcdb1ce02694ac5d7bcd6d791524b1dff
zboerger2 <zennith.boerger@gmail.com>
2023-04-12 17:09:15 -0500

    JSON successfully shuffles and randomizes enemies

--- JSONShuffler.py
+++ JSONShuffler.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import ijson
 import re
+import random
 
 __author__ = "Zennith Boerger"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
 _ENEMYCOUNT = 18028
-_ENEMYDISPLACEMENT = 15184
 
 class Enemy:
     def __init__(self, base_id, name, stats):
@@ -15,35 +15,37 @@ class Enemy:
         self.name = name
         self.stats = stats
 
-
+def get_id(enemy):
+    if enemy.base_id == '':
+        return -1
+    return int(enemy.base_id)
 
 def main():
     parser = ijson.parse(open(r'C:\Users\Zenni\Documents\GitHub\yakuza-7-randomizer\Test JSON\character_npc_soldier_personal_data.bin.json', r'r', encoding="utf8"))
     # there are 18027 npc's
     soldiers = []
     index_list = []
-    current_enemey = Enemy("", "", {})
+    current_enemy = Enemy("", "", {})
     old_progress = 0
     for prefix, event, value in parser:
         if event == 'end_map' and value == None:
-            index_list.append(prefix)
-            soldiers.append(current_enemey)
-            current_enemey = Enemy("", "", {})
+            soldiers.append(current_enemy)
+            current_enemy = Enemy("", "", {})
             
         if re.match(r'^[0-9]+$', prefix) and event == 'map_key':
             new_progress = int(re.search(r'\d+', prefix).group())/_ENEMYCOUNT
-            if (new_progress - old_progress) >= 0.011:
+            if (new_progress - old_progress) >= 0.1:
                 print(f'Progress {new_progress}')
                 old_progress = new_progress
-            current_enemey.name = value
-            current_enemey.base_id = re.search(r'\d+', prefix).group()
+            current_enemy.name = value
+            current_enemy.base_id = re.search(r'\d+', prefix).group()
 
-        if current_enemey.name != "" and "." in prefix:
+        if current_enemy.name != "" and "." in prefix:
             breakoff = 0
             while (prefix[breakoff-1] != "."):
                 breakoff -= 1
             key = prefix[breakoff:]
-            current_enemey.stats[key] = value
+            current_enemy.stats[key] = value
         # We need to store stats and names so we can create an Enemy object when its a valid enemy.
         # We need to keep track of the stats we get and the name, then add them into the list if they are valid
         # When we determine we are at a new enemy we need to redo the process
@@ -51,13 +53,67 @@ def main():
     for s in soldiers:
         if s.base_id == "" or s.name == "" or s.stats == {}:
             continue
-        if s.stats['hp'] > 0:
-            valid_soldiers.append(Enemy(s.base_id, s.name, s.stats))
-    for s in valid_soldiers:
-        print("base_id = {}, name = {}, hp = {}".format(s.base_id, s.name, s.stats['hp']))
-    print(len(valid_soldiers))
-    ## parse JSON file, obtain dictionary of all valid soldier npcs
-    ## then shuffle order of dictionary and put the npcs back into the json file
+        if int(s.stats['hp']) > 0:
+            valid_soldiers.append(s)
+            index_list.append(s.base_id)
+    # print(index_list)
+    index_list.sort()
+    random.shuffle(valid_soldiers)
+
+    enemy_blocks = {}
+    for i in range(len(index_list)):
+        enemy_block = f'\t\"{index_list[i]}\": {"{"}\n'
+        soldiers[soldiers.index(valid_soldiers[i])].base_id = index_list[i]
+        enemy_block += f'\t\t\"{valid_soldiers[i].name}\": {"{"}\n\t\t\t'
+        for stat in valid_soldiers[i].stats:
+            value = valid_soldiers[i].stats[stat]
+            if value == "reARMP_rowIndex":
+                continue
+            if stat == "reARMP_rowIndex":
+                end_comma = ","
+                if index_list[i] == '18027':
+                    end_comma = ""
+                enemy_block += f'\"{stat}\": {value}\n\t\t{"}"}\n\t{"}"}{end_comma}\n'
+            elif stat == "reARMP_isValid":
+                enemy_block += f'\"{stat}\": \"{value}\",\n\t\t\t'
+            else:
+                enemy_block += f'\"{stat}\": {value},\n\t\t\t'
+        enemy_blocks[valid_soldiers[i].name] = enemy_block
+    
+    fr = open(r'C:\Users\Zenni\Documents\GitHub\yakuza-7-randomizer\Test JSON\character_npc_soldier_personal_data.bin.json', r'r', encoding="utf8")
+    fw = open(r'character_npc_soldier_personal_data.bin.json', r'w', encoding="utf8")
+    line = ""
+    while True:
+        line = fr.readline()
+        if "\"1\": {" in line:
+            break
+        fw.write(line)
+    soldiers.sort(key=get_id)
+    current_id = 0
+    for s in soldiers:
+        if s.base_id == "" or s.name == "" or s.stats == {}:
+            continue
+        if s.stats['hp'] == 0:
+            enemy_block = f'\t\"{s.base_id}\": {"{"}\n'
+            enemy_block += f'\t\t\"{s.name}\": {"{"}\n\t\t\t'
+            for stat in s.stats:
+                value = s.stats[stat]
+                if value == "reARMP_rowIndex":
+                    continue
+                if stat == "reARMP_rowIndex":
+                    enemy_block += f'\"{stat}\": {value}\n\t\t{"}"}\n\t{"}"},\n'
+                elif stat == "reARMP_isValid":
+                    enemy_block += f'\"{stat}\": \"{value}\",\n\t\t\t'
+                else:
+                    enemy_block += f'\"{stat}\": {value},\n\t\t\t'
+            fw.write(enemy_block)
+        else:
+            fw.write(enemy_blocks[s.name])
+        current_id += 1
+    fw.write("}")
+    fw.close()
+
+    print("Randomized!")
 
 
 if __name__ == "__main__":
