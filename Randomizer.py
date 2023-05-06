13f560c195741eaa18f0a49eb6271c79edb7c77f
Zennith Boerger <zennith.boerger@gmail.com>
2023-05-05 22:54:34 -0500

    Removed okay enemy ID's from IGNORED_IDS
    there was 3 ids that were actually ok enemies, I mistook them for cleaning robot parts, when they werent and simply shared a name id with them.

--- Randomizer.py
+++ Randomizer.py
@@ -20,9 +20,9 @@ _IGNORED_IDS = ['15363', # Mr. Masochist
                 '15603', # Normal Majima
                 '15604', # Normal Saejima
                 '15640', # Normal Kiryu
-                '15668', '17621', '17829', # Right Arm of Cleaning Robots
-                '15483', '17622', '17830', # Left Arm of Cleaning Robots 
-                '17828', '15216', '15648', # Cleaning Robots
+                '17621', '17829', # Right Arm of Cleaning Robots
+                '17622', '17830', # Left Arm of Cleaning Robots 
+                '17828', '15216', # Cleaning Robots
                 '17990' # True Final Millenium Tower Amon
                 ]
 
